2094feded015652178d108fdcf7c8f06fb41eca6
llc1234 <93080319+llc1234@users.noreply.github.com>
2024-06-06 22:31:26 +0200

    Update main.py

--- main.py
+++ main.py
@@ -1,73 +1,87 @@
 # CRYP-256
 
-switch = [55, 100, 48, 57, 97, 49, 101, 56, 99, 102, 50, 98, 51, 52, 53, 54]
+import sys
+import random
 
-def SwitchChar(text):
-    output = ""
-    for char in text:
-        if '0' <= char <= '9':
-            index = ord(char) - 48
-        elif 'a' <= char <= 'f':
-            index = ord(char) - 97 + 10
+
+class CRYP256:
+    def __init__(self):
+        self.key = ""
+        self.filename = ""
+        self.text = ""
+
+    def encrypt(self, message):
+        encrypted_message = bytearray()
+        key_length = len(self.key)
+        
+        for i in range(len(message)):
+            encrypted_byte = message[i] ^ ord(self.key[i % key_length])
+            encrypted_message.append(encrypted_byte)
+        
+        random.seed(self.key)
+        shuffled_indices = list(range(len(encrypted_message)))
+        random.shuffle(shuffled_indices)
+        
+        index_map = {i: shuffled_indices[i] for i in range(len(shuffled_indices))}
+        
+        reordered_message = bytearray(len(encrypted_message))
+        for original_index, shuffled_index in index_map.items():
+            reordered_message[shuffled_index] = encrypted_message[original_index]
+        
+        return reordered_message
+
+
+    def decrypt(self, encrypted_message):
+        random.seed(self.key)
+        shuffled_indices = list(range(len(encrypted_message)))
+        random.shuffle(shuffled_indices)
+        
+        index_map = {shuffled_indices[i]: i for i in range(len(shuffled_indices))}
+        
+        reordered_message = bytearray(len(encrypted_message))
+        for shuffled_index, original_index in index_map.items():
+            reordered_message[original_index] = encrypted_message[shuffled_index]
+        
+        decrypted_message = bytearray()
+        key_length = len(self.key)
+        for i in range(len(reordered_message)):
+            decrypted_byte = reordered_message[i] ^ ord(self.key[i % key_length])
+            decrypted_message.append(decrypted_byte)
+        
+        return decrypted_message
+    
+    def EncryptFile(self):
+        r1 = open(self.filename, "rb")
+        encrypted_message = self.encrypt(r1.read())
+
+        for i in range(9):
+            encrypted_message = self.encrypt(encrypted_message)
+        r1.close()
+
+        r2 = open(self.filename + ".CRYP256", "wb")
+        r2.write(encrypted_message)
+        r2.close()
+
+    def DecryptFile(self):
+        r1 = open(self.filename, "rb")
+        encrypted_message = self.decrypt(r1.read())
+
+        for i in range(9):
+            encrypted_message = self.decrypt(encrypted_message)
+        r1.close()
+
+        r2 = open(self.filename[0:self.filename.find(".CRYP256")], "wb")
+        r2.write(encrypted_message)
+        r2.close()
+
+    def ArgsStart(self):
+        self.filename = sys.argv[1]
+        self.key = sys.argv[2]
+
+        if self.filename.endswith(".CRYP256"):
+            self.DecryptFile()
         else:
-            output += char
-            continue
-        output += chr(switch[index])
-    return output
-
-def SwitchBackChar(text):
-    output = ""
-    for char in text:
-        ascii_val = ord(char)
-        if ascii_val in switch:
-            index = switch.index(ascii_val)
-            if index < 10:
-                output += chr(index + 48)
-            else:
-                output += chr(index - 10 + 97)
-        else:
-            output += char
-    return output
+            self.EncryptFile()
 
 
-def encrypt(text):
-    encrypted_hex = ""
-    key_index = 0
-    for char in text:
-        ascii2 = ord(char)
-        ascii1 = ord(key[key_index])
-        encrypted_char = ascii1 ^ ascii2
-        encrypted_hex += format(encrypted_char, '02x')
-        key_index = (key_index + 1) % len(key)
-    return encrypted_hex
-
-def decrypt(encrypted_text):
-    decrypted_text = ""
-    key_index = 0
-    for i in range(0, len(encrypted_text), 2):
-        hex_pair = encrypted_text[i:i+2]
-        encrypted_char = int(hex_pair, 16)
-        ascii2 = ord(key[key_index])
-        decrypted_char = chr(encrypted_char ^ ascii2)
-        decrypted_text += decrypted_char
-        key_index = (key_index + 1) % len(key)
-    return decrypted_text
-
-
-
-
-text = "hello"
-
-key = "P9HebdWQwqzgT4YUDpQkd6T6dd8JKNCJD2DSdklSnldDDnekcnVLFHBERBX"
-en = encrypt(text)
-s = SwitchChar(en)
-sb = SwitchBackChar(s)
-de = decrypt(sb)
-
-
-
-print(f"raw text      : {text}")
-print(f"encrypt text  : {en}")
-print(f"switch        : {s}")
-print(f"switch Back   : {sb}")
-print(f"decrypt text  : {de}")
+CRYP256().ArgsStart()
