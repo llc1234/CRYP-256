82600aa93851bf260a9059959fba2361970a8ef5
llc1234 <93080319+llc1234@users.noreply.github.com>
2024-06-07 00:06:14 +0200

    Update CRYP256.py

--- CRYP256.py
+++ CRYP256.py
@@ -1,6 +1,6 @@
 # CRYP-256
 
-# python main.py secret_file.txt 5e884898da28047151d0e56f8dc6292773603d0d6aabbdd62a11ef721d1542d8
+# python CRYP256.py secret_file.txt 5e884898da28047151d0e56f8dc6292773603d0d6aabbdd62a11ef721d1542d8
 
 import os
 import sys
